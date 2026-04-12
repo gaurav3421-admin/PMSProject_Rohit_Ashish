dc78a103a563bce75970dc615cc23b9a62536389
Gaurav Kumar <gaurav3421@gmail.com>
2026-04-12 12:27:59 +0530

    Angular

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http'; // Step 1 - Import HttpClient and HttpErrorResponse
 import { ProductDetailsArray } from '../interfaces/product';
-import { Observable, of  } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -16,12 +16,12 @@ export class ProductService {
   private _productItem: Observable<ProductDetailsArray> | null = null;
 
 
- // GET Method to fetch product details by ID
+  // GET Method to fetch product details by ID
   getAllProductDetails(): Observable<ProductDetailsArray[]> {
     console.log("This is service Method :getAllProducts() calling");
     if (this.httpClientRequest) {
-      
-       // Return array of object (Product) 
+
+      // Return array of object (Product) 
       // T - ProductItem[]
       // Return  Observable <T>  : Observable <ProductItem[]> 
       return this.httpClientRequest.get<ProductDetailsArray[]>(this.apiURLProductDetails);
@@ -31,33 +31,33 @@ export class ProductService {
       return of([]);
     }
 
- }
+  }
 
- // GET Method to fetch product details by ID
- getProductDetailsByID( productId: number): void {
-  
- }
- // GET Method to search product details
- SearchProductDetails( searchText:string) :void {
+  // GET Method to fetch product details by ID
+  getProductDetailsByID(productId: number): void {
 
- }
+  }
+  // GET Method to search product details
+  SearchProductDetails(searchText: string): void {
 
- // POST Method to insert product details
- InsertProductDetails( productData: any): void {
+  }
 
- }
+  // POST Method to insert product details
+  InsertProductDetails(productData: any): void {
 
- // DELETE Method to remove product details by ID
- DeleteProductDetailsByID( productId: number): void {
-  
- }
+  }
 
- // PUT Method to update product details by ID
- UpdateProductDetailsByID( productId: number): void {
-  
- }
+  // DELETE Method to remove product details by ID
+  DeleteProductDetailsByID(productId: number): void {
 
- 
+  }
+
+  // PUT Method to update product details by ID
+  UpdateProductDetailsByID(productId: number): void {
+
+  }
+
+
 
 
 
